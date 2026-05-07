7ef1fe3d8c119f36c83085d591a3d5a2d6accdee
Le bao <leb61187@gmail.com>
2026-05-07 23:37:10 +0700

    List

--- code21.py
+++ code21.py
@@ -1,6 +1,6 @@
 nums = [3, 7, 2, 9, 7, 5]
 
-print("Số đứng đầu list", nums[0])
+print("Số đứng đầu list", nums[-1]) #(nums[-1]) flex hơn
 print("Số đứng cuối list", nums[5])
 print("Độ dài list là:", len(nums))
 total = 0
