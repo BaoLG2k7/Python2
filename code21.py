de3234cb338a08c5ba7ef4a9312cf2a25e39b40c
Le bao <leb61187@gmail.com>
2026-05-07 23:34:14 +0700

    List

---
+++ code21.py
@@ -0,0 +1,11 @@
+nums = [3, 7, 2, 9, 7, 5]
+
+print("Số đứng đầu list", nums[0])
+print("Số đứng cuối list", nums[5])
+print("Độ dài list là:", len(nums))
+total = 0
+for i in nums:
+    total = total + i
+
+print("Tổng các số trong list là:", total)
+
