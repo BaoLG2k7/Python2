29a849bbacbad847e08706c6be80f776a25122fd
Le bao <leb61187@gmail.com>
2026-05-11 22:26:21 +0700

    Căn bậc 2

---
+++ code28.py
@@ -0,0 +1,12 @@
+import math # mở hộp công cụ toán 
+
+A = [170, 60]
+B = [171, 61]
+
+distance = math.sqrt( # square root: căn bậc hai 
+    (B[0] - A[0])**2 +
+    (B[0] - A[0])**2
+)
+
+print(distance)
+
