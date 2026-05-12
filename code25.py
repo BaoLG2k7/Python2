f0e4233ad13add3406e12874495f1cc47ca27037
Le bao <leb61187@gmail.com>
2026-05-12 21:34:53 +0700

    thư viện

---
+++ code25.py
@@ -0,0 +1,14 @@
+student = {
+    "name": "Bao",
+    "age": 19,
+    "major": "AI"
+}
+
+print(student["name"])
+print(student["age"])
+print(student["major"])
+
+student["gpa"] = 3.8
+student["age"] = 20
+
+print(student)
